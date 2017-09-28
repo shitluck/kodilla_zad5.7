78feeb86f9a1e1d39c44f468c11b8131495ae77c
Mac <fuji.two6@gmail.com>
2017-09-28 11:33:47 +0200

    add files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '904914946495-ugobbkqe848pcdgak24h22pn3m55qaet.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'T7qJkgI4R5hwc989R_iINONU',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
